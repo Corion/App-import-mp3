57e33d50b330f0e91ea66f7cfbc90f52bc10f409
Max Maischein <corion@corion.net>
2020-12-30 09:52:09 +0100

    Move tag-reading code into its own subroutine

--- lib/File/Audio/CleanName.pm
+++ lib/File/Audio/CleanName.pm
@@ -36,6 +36,29 @@ sub sanitize( $pathname ) {
     return $real_name
 };
 
+sub audio_info( $audiofile, $artist=undef, $album=undef ) {
+    # Maybe this can take over MP3 too?
+    my $tag = Music::Tag->new( $audiofile);
+
+    $tag->get_tag;
+
+    # Mush 03/10 into 03
+    if( $tag->track =~ m!(\d+)\s*/\s*\d+$! ) {
+        $tag->track( $1 );
+    };
+
+    my %info = map { $_ => $tag->$_() } qw(artist album track title duration);
+    $info{ duration } ||= '-1'; # "unknown" if we didn't find anything
+    $audiofile =~ /\.(\w+)$/;
+    $info{ ext } = lc $1;
+
+    $info{ artist } //= $artist;
+    $info{ album  } //= $album;
+    $info{ track  } = sprintf '%02d', $info{ track };
+
+    return \%info;
+}
+
 =head2 C<< build_name >>
 
   my $clean_name = build_name( 'foo.mp3', '${artist} - ${album} - ${track} - ${title}.${ext}' );
@@ -47,24 +70,8 @@ sub build_name( $audiofile, $pattern='${artist} - ${album} - ${track} - ${title}
                 $artist=undef,
                 $album=undef ) {
     my $real_name;
-    # Maybe this can take over MP3 too?
-    my $tag = Music::Tag->new( $audiofile);
-
-    $tag->get_tag;
-
-    # Mush 03/10 into 03
-    if( $tag->track =~ m!(\d+)\s*/\s*\d+$! ) {
-        $tag->track( $1 );
-    };
-
-    my %info = map { $_ => $tag->$_() } qw(artist album track title);
-    $audiofile =~ /\.(\w+)$/;
-    $info{ ext } = lc $1;
-
-    $info{ artist } //= $artist;
-    $info{ album  } //= $album;
-    $info{ track  } = sprintf '%02d', $info{ track };
-    $real_name = ($pattern =~ s!\$\{\s*(\w+)\s*\}!$info{$1} // $1!gre);
+    my $info = audio_info( $audiofile, $artist, $album );
+    $real_name = ($pattern =~ s!\$\{\s*(\w+)\s*\}!$info->{$1} // $1!gre);
 
     return $real_name
 };
